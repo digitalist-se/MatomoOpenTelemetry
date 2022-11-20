f7e0bdd41d8c7e0bbf6ea860fc52da9afe43486b
Mikke Schirén <mikke.schiren@digitalistgroup.com>
2022-11-20 15:07:40 +0100

    basic examples

--- OpenTelemetry.php
+++ OpenTelemetry.php
@@ -8,20 +8,115 @@
 
 namespace Piwik\Plugins\OpenTelemetry;
 
+use GuzzleHttp\Client;
+use Monolog\Handler\StreamHandler;
+use OpenTelemetry\Contrib\Otlp\OtlpHttpTransportFactory;
+use Monolog\Logger;
+use GuzzleHttp\Psr7\HttpFactory;
+use OpenTelemetry\Contrib\Jaeger\Exporter as JaegerExporter;
+use OpenTelemetry\SDK\Trace\Sampler\AlwaysOnSampler;
+use OpenTelemetry\SDK\Trace\Span;
+use OpenTelemetry\SDK\Trace\SpanProcessor\SimpleSpanProcessor;
+use OpenTelemetry\SDK\Trace\TracerProvider;
+use OpenTelemetry\SDK\Trace\TracerProviderFactory;
+use OpenTelemetry\SDK\Common\Time\ClockFactory;
+use OpenTelemetry\SDK\Logs\SimplePsrFileLogger;
+use OpenTelemetry\SDK\Trace\SpanExporter\LoggerDecorator;
+use OpenTelemetry\SDK\Trace\SpanProcessor\BatchSpanProcessor;
+use OpenTelemetry\SDK\Trace\SpanExporter\ConsoleSpanExporter;
+use OpenTelemetry\Contrib\Otlp\SpanExporter;
+use OpenTelemetry\SDK\Common\Log\LoggerHolder;
+
+
 class OpenTelemetry extends \Piwik\Plugin
 {
+
+
+     /**
+     * These are the events that we want to use.
+     */
     public function registerEvents()
     {
         return [
-            'CronArchive.getArchivingAPIMethodForPlugin' => 'getArchivingAPIMethodForPlugin',
+            'CronArchive.init.start' => 'CronArchiveStart',
+            'CronArchive.end' => 'CronArchiveStop',
+            'API.Request.dispatch' => 'APIRequestDispatch'
         ];
     }
 
     // support archiving just this plugin via core:archive
-    public function getArchivingAPIMethodForPlugin(&$method, $plugin)
+    public function CronArchiveStart($array)
     {
-        if ($plugin == 'OpenTelemetry') {
-            $method = 'OpenTelemetry.getExampleArchivedMetric';
+        LoggerHolder::set(new Logger('otlp', [new StreamHandler('php://stderr')]));
+        $transport = (new OtlpHttpTransportFactory())->create('http://otel-collector:4318/v1/traces', 'application/x-protobuf');
+        $exporter = new SpanExporter($transport);
+
+        $tracerProvider =  new TracerProvider(
+            new SimpleSpanProcessor(
+                $exporter
+            )
+        );
+        $tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php');
+
+        $root = $span = $tracer->spanBuilder('root')->startSpan();
+        $scope = $span->activate();
+        try {
+            $root->addEvent('cron')->setAttribute('status', 'started');
+        } finally {
+            $root->end();
+            $scope->detach();
         }
+
+        $tracerProvider->shutdown();
     }
+    // support archiving just this plugin via core:archive
+    public function CronArchiveStop($array)
+    {
+        LoggerHolder::set(new Logger('otlp', [new StreamHandler('php://stderr')]));
+        $transport = (new OtlpHttpTransportFactory())->create('http://otel-collector:4318/v1/traces', 'application/x-protobuf');
+        $exporter = new SpanExporter($transport);
+
+        $tracerProvider =  new TracerProvider(
+            new SimpleSpanProcessor(
+                $exporter
+            )
+        );
+        $tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php');
+
+        $root = $span = $tracer->spanBuilder('root')->startSpan();
+        $scope = $span->activate();
+        try {
+            $root->addEvent('cron')->setAttribute('status', 'stopped');
+        } finally {
+            $root->end();
+            $scope->detach();
+        }
+        $tracerProvider->shutdown();
+    }
+
+    public function APIRequestDispatch(&$finalParameters, $pluginName, $methodName)
+    {
+        LoggerHolder::set(new Logger('otlp', [new StreamHandler('php://stderr')]));
+        $transport = (new OtlpHttpTransportFactory())->create('http://otel-collector:4318/v1/traces', 'application/x-protobuf');
+        $exporter = new SpanExporter($transport);
+
+        $tracerProvider =  new TracerProvider(
+            new SimpleSpanProcessor(
+                $exporter
+            )
+        );
+        $tracer = $tracerProvider->getTracer('io.opentelemetry.contrib.php');
+
+        $root = $span = $tracer->spanBuilder('APIRequestDispatch')->startSpan();
+        $scope = $span->activate();
+        try {
+            $root->addEvent('APIRequestDispatch')->setAttribute('status', 'dispatched');
+        } finally {
+            $root->end();
+            $scope->detach();
+        }
+        $tracerProvider->shutdown();
+    }
+
+
 }
--- README.md
+++ README.md
@@ -1,10 +1,36 @@
 # Matomo OpenTelemetry Plugin
+This needs PHP 8.0.x or above.
 
 ## Install needed packages
 
 ```bash
 composer require --ignore-platform-reqs --dev php-http/guzzle7-adapter
+composer require --ignore-platform-reqs --dev open-telemetry/opentelemetry
 ```
+
+or
+
+add to composer.json:
+
+```bash
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "git@github.com:open-telemetry/opentelemetry-php.git"
+        }
+    ],
+```
+
+and run:
+
+```bash
+composer require --dev  --ignore-platform-reqs open-telemetry/opentelemetry:dev-main#116e46d964e647a287096b8226ab415558f9d68e
+
+```
+
+## Activate plugin
+```
+
 
 ## Description
 
